485f8f41701b139fd2ddd15066bad6f9fe4050f0
mati108@gmail.com <34651529+matbenitez@users.noreply.github.com>
2018-10-27 10:56:15 -0300

    Fix issue #7
    https://github.com/AdvancedAlgos/KeyVaultModule/issues/7

--- server/app.js
+++ server/app.js
@@ -4,11 +4,9 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const AUTH_CONFIG = require('./auth/Auth0');
-const dbConfig = require('./auth/dbConfig')
+const dbConfig = require('./config/dbConfig')
 const logger = require('./config/logger')
+
 const app = express();
 
 // Allow cross origing request
@@ -21,36 +19,12 @@ mongoose.connection.once('open',()=>{
   logger.info('Connected to the DB.');
 })
 
-/* Here we bind all requests to this endpoint to be procecced by the GraphQL Library. */
-
-
-app.use('/graphql',
-  jwt({
-    // Dynamically provide a signing key
-    // based on the kid in the header and
-    // the signing keys provided by the JWKS endpoint.
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 1,
-      jwksUri: `https://${AUTH_CONFIG.domain}/.well-known/jwks.json`
-    }),
-
-    // Validate the audience and the issuer.
-    credentialsRequired: false,
-    audience: AUTH_CONFIG.api_audience,
-    issuer: AUTH_CONFIG.issuer,
-    algorithms: [`RS256`]
-  }),
-  function (req, res, next) {
-    return next()
-  }
-);
-
 app.use('/graphql', graphqlHTTP(req => {
   return {
     schema: schema,
-    context: { user: req.user },
+    context: {
+      userId: req.headers.userid
+    },
     graphiql: true
   }
 }))
