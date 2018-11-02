908d818bf3f41cb775541c5a453d2a5387099d7e
Nikola Bjelo <nikola@bjelo.ch>
2018-11-02 20:44:42 +0100

    Hotfix (unchecked)

--- server/src/index.js
+++ server/src/index.js
@@ -5,8 +5,18 @@ import schema from './schema'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import logger from './config/logger'
+import wrongPreshared from './errors/notAllowed.json';
 
 const app = express()
+
+app.post('/graphql', (req, res, next) => {
+  if (req.headers.preshared === process.env.PRESHARED_GATEWAY_KEY) {
+    next();
+  } else {
+    res.send(wrongPreshared);
+  }
+});
+
 
 // Allow cross origing request
 app.use(cors())
